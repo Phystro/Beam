cef30f625048674a2e14fb1b817f5278d8ce6a7f
Anthony Karoki <phystroq@gmail.com>
2020-02-07 01:38:05 +0300

    depracated status progress

--- beam.h
+++ beam.h
@@ -10,10 +10,10 @@
 #define DEFAULT_OUT_NON_ROOT "$HOME/Downloads/"
 
 double now();
+void error(char *report);
 long int filesize(const char *filename);
 void beamRate(u_int64_t nbytes, double ntime);
 void ratioProgress(u_int64_t curr_item, u_int64_t total_iter, double timeFrame);
-void error(char *report);
 void BeamServer(char *filepath, char *server_ip);
 void BeamClient(char *filedest, char *server_ip);
 
